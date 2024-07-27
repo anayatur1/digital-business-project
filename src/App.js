f33be63c95369d8674c7133648caf8a260682247
anayatur1 <119777828+anayatur1@users.noreply.github.com>
2024-07-27 16:24:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Layout from './Components/Layouts/Layout';
+import Calender from "./pages/Calender/Calender"
+import Boards from './pages/Board/Boards';
+
+function App() {
+  return (
+    <div id="dashboard">
+      <BrowserRouter>
+       <Routes>
+         <Route path="/" element={<Layout/>}>
+         <Route path="Dashboard" element={<Dashboard/>}></Route>
+         <Route path="Calender" element={<Calender/>}></Route>
+         <Route path="Board" element={<Boards/>}></Route>
+         </Route>
+         
+       </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
